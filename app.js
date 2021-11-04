5bdc346758f2dfe7dd8135cba1474ea7164838fa
basokant <basokanthan@gmail.com>
2021-11-04 12:48:55 -0400

    add clear button and resize grid functionality

--- app.js
+++ app.js
@@ -5,15 +5,35 @@ let currentSize = DEFAULT_SIZE;
 let currentColor = DEFAULT_COLOR;
 
 const grid = document.querySelector(".grid");
-grid.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
-grid.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`;
 
-for (let i = 0; i < currentSize * currentSize; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.addEventListener("mouseover", changeColor);
-    grid.appendChild(gridElement);
+const clearBtn = document.querySelector("button");
+clearBtn.addEventListener("click", function() {
+    let size = prompt("Enter a grid size between 1 and 100");
+    if (size <= 100 && size >= 1) {
+        currentSize = size;
+        gridSetup(currentSize);
+    }
+})
+
+gridSetup(currentSize);
+
+function gridSetup(size){
+    clearGrid();
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.addEventListener("mouseover", changeColor);
+        grid.appendChild(gridElement);
+    }
 }
 
 function changeColor(e) {
     e.target.style.backgroundColor = currentColor;
+}
+
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
 }
